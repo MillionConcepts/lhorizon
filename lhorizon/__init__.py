f7966b0c6c8de8c0d51bba09f0bbdb8866592f5b
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2021-07-06 13:44:14 -0400

    tweak

--- lhorizon/__init__.py
+++ lhorizon/__init__.py
@@ -1,2 +1,3 @@
 from lhorizon.base import LHorizon
 
+__version__ = "1.0"
--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name="lhorizon", version="0.5.0a0", packages=find_packages())
-
 setup(
     name="lhorizon",
     version="1.0.0",
